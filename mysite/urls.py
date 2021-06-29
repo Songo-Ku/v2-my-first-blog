f08b2fa6c1c66b00d4c4a568993740485f784cfb
songo-ku <karczewski49@gmail.com>
2021-06-29 18:51:36 +0200

    Resolve conflicts

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,9 +23,4 @@ urlpatterns = [
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
-<<<<<<< HEAD
-=======
-
->>>>>>> srodek podstaw djangogirls, posts, detail
 ]
-
